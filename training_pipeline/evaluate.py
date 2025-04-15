57b6a42ebbfc4e59f0150a5571dbbbb291f32568
Aqsa-K <aqsakausar30@gmail.com>
2025-04-15 11:08:52 +0500

    testing evaluation script

--- training_pipeline/evaluate.py
+++ training_pipeline/evaluate.py
@@ -23,42 +23,50 @@ accs = []
 
 dataset = load_dataset("AqsaK/1880_census_handwritten_archives", split="train")
 
-for idx, sample in tqdm(enumerate(dataset), total=len(dataset)):
-    # prepare encoder inputs
-    pixel_values = processor(sample["image"].convert("RGB"), return_tensors="pt").pixel_values
-    pixel_values = pixel_values.to(device)
-    # prepare decoder inputs
-    task_prompt = "<s_cord-v2>"
-    decoder_input_ids = processor.tokenizer(task_prompt, add_special_tokens=False, return_tensors="pt").input_ids
-    decoder_input_ids = decoder_input_ids.to(device)
-    
-    # autoregressively generate sequence
-    outputs = model.generate(
-            pixel_values,
-            decoder_input_ids=decoder_input_ids,
-            max_length=model.decoder.config.max_position_embeddings,
-            early_stopping=True,
-            pad_token_id=processor.tokenizer.pad_token_id,
-            eos_token_id=processor.tokenizer.eos_token_id,
-            use_cache=True,
-            num_beams=1,
-            bad_words_ids=[[processor.tokenizer.unk_token_id]],
-            return_dict_in_generate=True,
-        )
+for i in tqdm(range(20)):
+    try:
+        sample = dataset[i]
+        print(k)
+        print(sample)
 
-    # turn into JSON
-    seq = processor.batch_decode(outputs.sequences)[0]
-    seq = seq.replace(processor.tokenizer.eos_token, "").replace(processor.tokenizer.pad_token, "")
-    seq = re.sub(r"<.*?>", "", seq, count=1).strip()  # remove first task start token
-    seq = processor.token2json(seq)
+# for idx, sample in tqdm(enumerate(dataset), total=len(dataset)):
+        # prepare encoder inputs
+        pixel_values = processor(sample["image"].convert("RGB"), return_tensors="pt").pixel_values
+        pixel_values = pixel_values.to(device)
+        # prepare decoder inputs
+        task_prompt = "<s_cord-v2>"
+        decoder_input_ids = processor.tokenizer(task_prompt, add_special_tokens=False, return_tensors="pt").input_ids
+        decoder_input_ids = decoder_input_ids.to(device)
+        
+        # autoregressively generate sequence
+        outputs = model.generate(
+                pixel_values,
+                decoder_input_ids=decoder_input_ids,
+                max_length=model.decoder.config.max_position_embeddings,
+                early_stopping=True,
+                pad_token_id=processor.tokenizer.pad_token_id,
+                eos_token_id=processor.tokenizer.eos_token_id,
+                use_cache=True,
+                num_beams=1,
+                bad_words_ids=[[processor.tokenizer.unk_token_id]],
+                return_dict_in_generate=True,
+            )
 
-    ground_truth = json.loads(sample["ground_truth"])
-    ground_truth = ground_truth["gt_parse"]
-    evaluator = JSONParseEvaluator()
-    score = evaluator.cal_acc(seq, ground_truth)
+        # turn into JSON
+        seq = processor.batch_decode(outputs.sequences)[0]
+        seq = seq.replace(processor.tokenizer.eos_token, "").replace(processor.tokenizer.pad_token, "")
+        seq = re.sub(r"<.*?>", "", seq, count=1).strip()  # remove first task start token
+        seq = processor.token2json(seq)
 
-    accs.append(score)
-    output_list.append(seq)
+        ground_truth = json.loads(sample["ground_truth"])
+        ground_truth = ground_truth["gt_parse"]
+        evaluator = JSONParseEvaluator()
+        score = evaluator.cal_acc(seq, ground_truth)
+
+        accs.append(score)
+        output_list.append(seq)
+    except Exception as e:
+        print(f"Skipping bad image: {e}")
 
 scores = {"accuracies": accs, "mean_accuracy": np.mean(accs)}
 print(scores, f"length : {len(accs)}")
